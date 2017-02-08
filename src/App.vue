9cf8c4c2471826f3b9501da1deb4915b79703aff
redstoone <cy_jing_2010@163.com>
2017-02-08 22:16:09 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="app">
+        <img src="./assets/logo.png">
+        <h1>{{ msg }}</h1>
+        <mt-button @click.native="handleClick">Let's do it</mt-button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'app',
+    data () {
+        return {
+            msg: 'Welcome to Your Vue.js App'
+        }
+    },
+    methods: {
+        handleClick: function() {
+            this.$toast('Hello world!')
+        }
+    }
+}
+</script>
